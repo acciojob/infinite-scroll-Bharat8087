e90e54cfa6f1e561c19a66ceac6a21bed21cec4d
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 12:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function() {
+ list = document.getElementById("myList");
+    for (let i = 1; i <= 10; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = "List Item " + i;
+        list.appendChild(listItem);
+    }
+    function addMoreItems() {
+        const items = list.querySelectorAll("li");
+        const lastItem = items[items.length - 1];
+        const rect = lastItem.getBoundingClientRect();
+        if (rect.bottom <= window.innerHeight) {
+            for (let i = 1; i <= 2; i++) {
+                const newItem = document.createElement("li");
+                newItem.textContent = "List Item " + (items.length + i);
+                list.appendChild(newItem);
+            }
+        }
+    }
+    window.addEventListener("scroll", addMoreItems);
+});
 
